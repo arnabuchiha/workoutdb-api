ebb2a51ce5414cbe9d36decf8f574d5f0a0317f0
arnabuchiha <arnabkeya04@gmail.com>
2025-06-23 18:17:20 +0530

    open api auth

--- src/controllers/docsController.ts
+++ src/controllers/docsController.ts
@@ -3,6 +3,7 @@ import { Scalar } from "@scalar/hono-api-reference";
 
 export const scalarDocs = Scalar({
   url: "/static/openapi.yaml",
+  layout: "modern",
   theme: "deepSpace",
   pageTitle: "Workout Database API",
 });
